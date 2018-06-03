195be23b1d3fe2c7820df6e92680398581f1defc
DanieleLupo <34913817+DanieleLupo@users.noreply.github.com>
2018-06-03 15:33:35 +0200

    Aggiunto check sull'input al login

--- gestoreCookie.js
+++ gestoreCookie.js
@@ -1,22 +1,18 @@
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function creaCookie(nome, valore){
+    return [nome,'=',JSON.stringify(valore),'; domain=', window.location.host.toString(),'; path=/;'].join('');
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+function leggiCookie(nome){
+    var result = document.cookie.match(new RegExp(nome + '=([^;]+)'));
+    result && (result = JSON.parse(result[1]));
+    return result;
+}
+
+function eliminaCookie(nome){
+    document.cookie = [nome, '=; expires=Thu 01-Jan-1970 00:00:01 GMT; path=/; domain=.', window.location.host.toString()].join('');
+}
+
+function logout(nome){
+    eliminaCookie(nome);
+    window.location.href = "./login.html";
 }
--- login.js
+++ login.js
@@ -1,15 +1,27 @@
-
+if(leggiCookie('utente') != null){
+    window.location = "pannello.html";
+}
 
 var utenti = Array();
 let utente = {'Nome':'Daniele','Cognome':'Lupo','CF':'LPUDNL94D20A489M','ComuneDiNascita':'Atripalda','DataDiNascita':'20/04/1994','Indirizzo':'Via Traversa I','CAP':'83100','Cellulare':'3297270185','Email':'d.lupo1@studenti.unisa.it','CodiceOperatore':'0123456789','ZonaComando':'Avellino','Password':'Password'};
 utenti.push(utente);
 
-console.log(utenti);
-
 function login(){
     // Il controllo dei campi viene fatto dall'HTML
-    let codiceOperatore = document.getElementById('inputCodiceOperatore').value;
-    let password = document.getElementById('inputPassword').value;
+    let codiceOperatore = document.getElementById('inputCodiceOperatore').value.trim();
+    codiceOperatore = codiceOperatore.replace(" ","");
+    if(codiceOperatore.length == 0){
+        alert('Il codice operatore non può essere vuoto.');
+        document.getElementById('inputCodiceOperatore').value = document.getElementById('inputCodiceOperatore').value.trim();
+        return false;
+    }
+    let password = document.getElementById('inputPassword').value.trim();
+    password = password.replace(" ","");
+    if(password.length == 0){
+        alert('La password non può essere vuota.');
+        document.getElementById('inputPassword').value = document.getElementById('inputPassword').value.trim();
+        return false;
+    }
 
     // Cerco l'utente fra gli utenti
     let u;
@@ -18,12 +30,14 @@ function login(){
         u = utenti[i];
         //console.log(u);
         if(u.CodiceOperatore == codiceOperatore && u.Password == password){
-            var date = new Date();
-            var minutes = 60*8;
-            date.setTime(date.getTime() + (minutes * 60 * 1000));
-            setCookie('utente',u, date);
-            u = getCookie('utente');
-            console.log(u.Nome);
+            document.cookie = creaCookie('utente',u);
+            window.location = "pannello.html";
+            return;
         }
+        alert('Username o password errati.');
     }
+}
+
+function registrazione(){
+    alert('Oh yes!');
 }
